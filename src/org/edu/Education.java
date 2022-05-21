f4bd69ffd21a88c8ef4c37b1d6fc553eff97798d
Anusuya10 <anubbytaj2510@gmail.com>
2022-05-21 22:02:09 +0530

    sdg

--- src/org/edu/Education.java
+++ src/org/edu/Education.java
@@ -3,6 +3,9 @@ package org.edu;
 public class Education {
 	public void ug() {
 		System.out.println("ECE");
+
+		System.out.println("BCA");
+		System.out.println("mba");
 	}
 	public void pg() {
 		System.out.println("EEE");
